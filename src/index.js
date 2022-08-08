b67a522ea1b66a813b229936e4c3a79723ba3179
vcsil <vcsilva@poli.ufrj.br>
2022-08-08 04:52:47 -0300

    Criando gerenciador de rotas

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import dotenv from "dotenv";
 import chalk from "chalk";
 import cors from "cors";
 
+import router from "./routes/router.js";
+
 dotenv.config();
 
 const server = express();
 server.use(express.json());
 server.use(cors());
+
+server.use(router);
 
 server.listen(process.env.PORT, () => {
     console.log(
