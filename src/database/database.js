b60c357514a4723dd39dac79b989af39a4da5011
vcsil <vcsilva@poli.ufrj.br>
2022-08-08 18:43:43 -0300

    Prepare for deploy

--- src/database/database.js
+++ src/database/database.js
@@ -4,16 +4,14 @@ import pg from "pg";
 dotenv.config();
 
 const { Pool } = pg;
-const config = {
+
+const databaseConfig = {
     connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 };
 
-if (process.env.MODE === "PROD") {
-    config.ssl = {
-        rejectUnauthorized: false,
-    };
-}
-
-const connection = new Pool(config);
+const connection = new Pool(databaseConfig);
 
 export default connection;
