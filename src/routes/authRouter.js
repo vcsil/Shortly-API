3a8416e2daee8d287f91159449f9e8587d24635d
vcsil <vcsilva@poli.ufrj.br>
2022-08-08 05:28:40 -0300

    Refatorando middleware de validação do schema
    Substitui váiros arquivos

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,12 +1,19 @@
 import { Router } from "express";
 
-import userValidationSchema from "../middlewares/userValidationSchemaMiddleware.js";
+import validateSchema from "../middlewares/schemaValidator.js";
 import authEmailValidation from "../middlewares/authEmailValidatonMiddleware.js";
 import signUp from "../controllers/authController.js";
+import userSignUpSchema from "../Schemas/userSchema.js";
+import loginSchema from "../Schemas/logninSchema.js";
 
 const authRouter = Router();
 
-authRouter.post("/signup", userValidationSchema, authEmailValidation, signUp);
-authRouter.post("/signin");
+authRouter.post(
+    "/signup",
+    validateSchema(userSignUpSchema),
+    authEmailValidation,
+    signUp
+);
+authRouter.post("/signin", validateSchema(loginSchema));
 
 export default authRouter;
